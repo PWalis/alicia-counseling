fb74aa9b1cbb9db2d179cd10d8ad9af41f480941
Daisy Pennartz <daisypenn99@gmail.com>
2024-06-02 16:50:47 -0700

    Updates to font, hero section and UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lexend_Deca } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/ui/header/Header"
 
-const inter = Inter({ subsets: ["latin"] });
+const LexendDeca = Lexend_Deca({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={LexendDeca.className}>
         <Header /> 
         {children}</body>
     </html>
